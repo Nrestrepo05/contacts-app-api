6eb52625b2e5d770545ac8c71c7c92626b2b37cc
nrestrepo05 <nrestrepo.r05@gmail.com>
2021-05-06 10:37:17 -0400

    Add message error to required fields

--- components/contacts/model.js
+++ components/contacts/model.js
@@ -5,14 +5,14 @@ const { Schema } = mongoose;
 const ContactSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'name is required'],
     minLength: [2, 'name should have at least 2 characters'],
     maxLength: [70, 'name is too long'],
     match: [/^[A-Za-z\s]+$/, 'name can only contains letters'],
   },
   last_name: {
     type: String,
-    required: true,
+    required: [true, 'last name is required'],
     minLength: [2, 'last name should have at least 2 characters'],
     maxLength: [70, 'last name is too long'],
     match: [/^[A-Za-z\s]+$/, 'last name can only contains letters'],
@@ -29,7 +29,7 @@ const ContactSchema = new Schema({
   },
   email: {
     type: String,
-    unique: true,
+    unique: [true, 'email must be unique'],
     match: [/[\w-.]+@([\w-]+\.)+[\w-]{2,4}/, 'email must be valid'],
   },
 });
